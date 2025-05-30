926f71cd463c3df897ee18b4613efad37160e7f3
mdzakirhussain <zakir.hussain.work@gmail.com>
2025-05-30 23:26:55 +0530

    Update modelsim_install.sh

--- modelsim_install.sh
+++ modelsim_install.sh
@@ -9,3 +9,9 @@ sudo apt-get install -y libxext6:i386 libxft2:i386 libxrender1:i386 libxt6:i386 
 wget https://downloads.intel.com/akdlm/software/acdsinst/20.1std.1/720/ib_installers/ModelSimSetup-20.1.1.720-linux.run
 chmod +x ModelSimSetup-20.1.1.720-linux
 ./ModelSimSetup-20.1.1.720-linux.run --mode text
+
+set this
+
+   export PATH=/home/codespace/intelFPGA/20.1/modelsim_ase/bin:$PATH
+
+   source it later
